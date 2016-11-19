77478eee1e0643b49579f9d0faff27f9fb17c88c
Ellen Moore <ellenmoore13@gmail.com>
2016-11-18 20:10:32 -0600

    Update travel-left-rail.php

--- travel-left-rail.php
+++ travel-left-rail.php
@@ -1,4 +1,5 @@
-         <div class="panel panel-default">
+?>
+ <div class="panel panel-default">
            <div class="panel-heading">Search</div>
            <div class="panel-body">
              <form>
@@ -15,26 +16,24 @@
       
          <div class="panel panel-info">
            <div class="panel-heading">Continents</div>
-           <ul class="list-group">               
-              <li class="list-group-item"><a href="#">Asia</a></li>
-              <li class="list-group-item"><a href="#">Africa</a></li>
-              <li class="list-group-item"><a href="#">Europe</a></li>
-              <li class="list-group-item"><a href="#">North America</a></li>
-              <li class="list-group-item"><a href="#">South America</a></li>    
+           <ul class="list-group">  
+			<?php
+				//iterate through the Continent array then call the function to display the continent link
+				foreach ($continents as $link) {
+					continentLinks($link);
+				}
+			?>		 
            </ul>
          </div>  <!-- end continents panel -->  
          <div class="panel panel-info">
            <div class="panel-heading">Popular Countries</div>
-           <ul class="list-group">               
-              <li class="list-group-item"><a href="#">Bahamas</a></li>
-              <li class="list-group-item active">Canada</li>
-              <li class="list-group-item"><a href="#">Germany</a></li>
-              <li class="list-group-item"><a href="#">Ghana</a></li>
-              <li class="list-group-item"><a href="#">Greece</a></li>
-              <li class="list-group-item"><a href="#">Hungary</a></li>
-              <li class="list-group-item"><a href="#">Italy</a></li>
-              <li class="list-group-item"><a href="#">Spain</a></li>
-              <li class="list-group-item"><a href="#">United Kingdom</a></li>
-              <li class="list-group-item"><a href="#">United States</a></li>    
+           <ul class="list-group"> 
+
+			<?php
+				//iterate through the countries array then call the function to display the country link
+				foreach ($countries as $link) {
+					countryLinks($link);
+				}
+			?>	 
            </ul>
          </div>  <!-- end countries panel -->    
